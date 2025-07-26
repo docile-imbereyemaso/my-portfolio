c73f222ce5a26604053718d4d88a8a2b3678490e
Docile123 <imbereyemasoo@gmail.com>
2025-07-26 17:30:08 +0200

    Initial commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+        {/* Theme toggle */}
+
+
+        {/* Background Effect */}
+
+        {/* Navbar */}
+
+            {/* Main Content */}
+
+
+                  {/* Footer */}
+    </div>
+  )
+}
+
+export default Home
